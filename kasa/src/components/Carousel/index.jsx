a025174f724c767a7ef5cc17ab9d611897940a8e
Filiz <filizhakan13@gmail.com>
2022-11-08 19:16:24 +0100

    Fixed carousel

--- kasa/src/components/Carousel/index.jsx
+++ kasa/src/components/Carousel/index.jsx
@@ -17,27 +17,28 @@ function Carousel({ images })
         setCurrent(currentImage === allImages - 1 ? 0 : currentImage + 1)
     }
 
-    // If 1 image : classic display
-    if(allImages === 1)
+    // If more than one image
+    if(allImages > 1)
     {
         return (
         <div className="carousel">
             <button onClick={(prev)}>
-            <img className="carousel__icon--left" src={arrowLeft} alt="Precedent"/>
+            <img className="carousel__arrow--left" src={arrowLeft} alt="Precedent"/>
             </button>
             <img className="carousel__img" src={images[currentImage]} alt="Images"/>
             <button onClick={(next)}>
-                <p className="carousel__numbering">{currentImage + 1}/{allImages}</p>
-                <img className="carousel__icon--right" src={arrowRight} alt="Suivant"/>
+                <p className="carousel__index">{currentImage + 1}/{allImages}</p>
+                <img className="carousel__arrow--right" src={arrowRight} alt="Suivant"/>
             </button>
         </div>
         );
+
     } else
-    {
+    {   // if one image: classic display
         return(
             <div className="carousel">
                 <img className="carousel__img" src={images[currentImage]} alt="Images"/>
-                <p className="carousel__numbering">1/1</p>
+                <p className="carousel__index">1/1</p>
             </div>
         )
     }
