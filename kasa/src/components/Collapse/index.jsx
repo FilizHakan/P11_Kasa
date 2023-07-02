5dbeb9a70d49eb68999c9b08cb63776cf843ea21
Filiz <filizhakan13@gmail.com>
2023-07-02 13:42:07 +0200

    Fixed dropdown

--- kasa/src/components/Collapse/index.jsx
+++ kasa/src/components/Collapse/index.jsx
@@ -7,21 +7,21 @@ function Collapse(props)
     
     const [setActive, setActiveState] = useState("");
     const [setHeight, setHeightState] = useState("0px");
-    const [setRotate, setRotateState] = useState("open");
+    const [setRotate, setRotateState] = useState("closed");
     const content = useRef(null);
     
     function toggle()
     {
         setActiveState(setActive === "inactive" ? "active" : "inactive");
-        setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
-        setRotateState(setRotate === "open" ? "closed" : "open");
+        setHeightState(setActive === "inactive" ? "0px" : `${content.current.scrollHeight}px`);
+        setRotateState(setRotate === "closed" ? "open" : "closed");
     }
 
     return (
         <div className="collapseContainer">
             <button className={`collapseBtn ${setActive}`} onClick={toggle}>
                 <p className="collapseTitle">{props.title}</p>
-                <img className={`${setRotate}`} alt="open collapse" src={arrowDown} />
+                <img className={`${setRotate}`} alt="collapse" src={arrowDown} />
             </button>
 
             <div className="collapseContent" ref={content} style={{ maxHeight: `${setHeight}` }}>
